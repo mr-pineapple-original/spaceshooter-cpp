dbd2a35f8674859b67c19266b9ddbf6531a4d261
Bongo <bongopoyo@proton.me>
2025-11-08 16:23:43 +0500

    Some things

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,28 @@
-#include<stdio.h>
+// Normal
+#include <iostream>
+#include <math.h>
+#include <cstdlib>
+// SFML 
+#include <SFML/Window.hpp>
+#include <SFML/System.hpp>
+#include <SFML/Graphics.hpp>
 
-void main() {
+using namespace sf;
 
-  
+int main() {
+  RenderWindow window(sf::VideoMode({800, 600}), "SFML window");
+  window.setFramerateLimit(60);
+  const Texture texture("../sprites/player.png");
+  Sprite sprite(texture);
+  while (window.isOpen()) {
+    while (const std::optional event = window.pollEvent()) {
+      if (event->is<sf::Event::Closed>())
+        window.close();
+    }
+
+    window.clear();
+    window.draw(sprite);
+    window.display();
+  }
+
 }
