22a9bff86cabaf8fe68245386f09d31d7946a319
Mr Pineapple <pineapple123132@gmail.com>
2025-11-09 15:16:14 +0500

    Added player movement

--- src/main.cpp
+++ src/main.cpp
@@ -15,8 +15,12 @@ int main()
 {
   RenderWindow window(VideoMode({800, 600}), "SFML window");
   window.setFramerateLimit(60);
+
+
   const Texture texture("sprites/player.png");
   Sprite sprite(texture);
+  Vector2f position = {400, 300};
+
   while (window.isOpen()) 
   {
     while (const optional event = window.pollEvent()) 
@@ -24,6 +28,12 @@ int main()
       if (event->is<Event::Closed>())
         window.close();
     }
+
+    if (Keyboard::isKeyPressed(Keyboard::Key::Left))
+      position.x -= 5;
+    if (Keyboard::isKeyPressed(Keyboard::Key::Right))
+      position.x += 5;
+    sprite.setPosition(position);
     window.clear();
     window.draw(sprite);
     window.display();
