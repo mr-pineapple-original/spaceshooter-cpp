41faa494682ab40a7d3cc786a5c6e4f5f4b7ac44
Bongo <bongopoyo@proton.me>
2025-11-09 17:46:18 +0500

    Laser Code

--- src/game.hpp
+++ src/game.hpp
@@ -12,10 +12,20 @@ void game_uninitialize()
 void game_draw()
 {
     spaceship_draw(); // Draw the spaceship from the spaceship header file
+    //
+    for(auto& laser: spaceship_lasers)
+    {
+        laser.draw();
+    }
 }
 void game_update()
 {
     spaceship_boundaries();
+
+    for(auto& laser: spaceship_lasers) 
+    {
+        laser.update();
+    }
 }
 void handle_input()
 {
@@ -23,8 +33,12 @@ void handle_input()
     {
         spaceship_position.x = spaceship_position.x - 5;
     }
-    if(IsKeyDown(KEY_RIGHT))
+    else if(IsKeyDown(KEY_RIGHT))
     {
         spaceship_position.x = spaceship_position.x + 5;
+    }
+    else if(IsKeyDown(KEY_SPACE)) 
+    {
+        spaceship_fire_laser();
     }
 }
--- src/spaceship.hpp
+++ src/spaceship.hpp
@@ -1,7 +1,13 @@
 #pragma once // To include the library only once
 #include<raylib.h> // To render Graphics in this file
+#include"laser.hpp"
+#include<vector>
+
+
 Texture2D spaceship_image;
 Vector2 spaceship_position; // Position of Spaceship
+std::vector<Laser> spaceship_lasers;
+
 void spaceship_initialize()
 {
     spaceship_image = LoadTexture("sprites/player.png"); // Load the image and assing it to the image attribute
@@ -27,4 +33,7 @@ void spaceship_boundaries()
         spaceship_position.x=720;
     }
 }
-void spaceship_fire_laser();
+void spaceship_fire_laser()
+{
+   spaceship_lasers.push_back(Laser({spaceship_position.x + spaceship_image.width/2 -2,spaceship_position.y}-6)); 
+}
