940cf70fbfeab963eea0dce2ea4301a083046768
Bongo <bongopoyo@proton.me>
2025-11-14 15:26:53 +0500

    la la la

--- src/game.hpp
+++ src/game.hpp
@@ -8,42 +8,42 @@ std::vector<Obstacle>
                // store it and use it in game_initialize function
 std::vector<Alien> aliens;
 
-
-// To avoid cross referencing we put the create obstacle function in the game header file
-std::vector<Obstacle> obstacle_create()
-{
-    int obstacle_width = Obstacle({100, 100}).grid[0].size() * 3;
-    float obstacle_gap = (GetScreenWidth() - (4 * obstacle_width))/5.0f; // Takes the gap between two obstacles by taking the width of all the 4 obstacles and subtracting it from the screen width and dividing it by 5 cause for 4 obstacles you have to add 5 gaps ( 3 for each other and 2 for the parameters of the screen )
-    for( int i = 0; i!=4; i++ ) // Running it exactly 4 times to create 4 objects and later draw them
-    {
-        float obstacle_position_x = (i + 1) * obstacle_gap + i * obstacle_width; // To calculate the x position of the obstacle by ensuring they are evenly spaced
-        float obstacle_position_y = GetScreenHeight()-100;
-        obstacles.push_back(Obstacle({obstacle_position_x, obstacle_position_y}));
-    } 
-    return obstacles;
+// To avoid cross referencing we put the create obstacle function in the game
+// header file
+std::vector<Obstacle> obstacle_create() {
+  int obstacle_width = Obstacle({100, 100}).grid[0].size() * 3;
+  float obstacle_gap =
+      (GetScreenWidth() - (4 * obstacle_width)) /
+      5.0f; // Takes the gap between two obstacles by taking the width of all
+            // the 4 obstacles and subtracting it from the screen width and
+            // dividing it by 5 cause for 4 obstacles you have to add 5 gaps ( 3
+            // for each other and 2 for the parameters of the screen )
+  for (int i = 0; i != 4; i++) // Running it exactly 4 times to create 4 objects
+                               // and later draw them
+  {
+    float obstacle_position_x =
+        (i + 1) * obstacle_gap +
+        i * obstacle_width; // To calculate the x position of the obstacle by
+                            // ensuring they are evenly spaced
+    float obstacle_position_y = GetScreenHeight() - 100;
+    obstacles.push_back(Obstacle({obstacle_position_x, obstacle_position_y}));
+  }
+  return obstacles;
 }
 
-void game_uninitialize()
-{
-    
-}
-void game_draw()
-{
-    spaceship_draw(); // Draw the spaceship from the spaceship header file
-    //
-    for(auto& laser: spaceship_lasers)
-    {
-        laser.draw();
-    }
-    for(auto& obstacle : obstacles)
-    {
-        obstacle.draw();
-    }
-    for(auto& alien : aliens)
-    {
-      alien.draw();
-    }
-
+void game_uninitialize() {}
+void game_draw() {
+  spaceship_draw(); // Draw the spaceship from the spaceship header file
+  //
+  for (auto &laser : spaceship_lasers) {
+    laser.draw();
+  }
+  for (auto &obstacle : obstacles) {
+    obstacle.draw();
+  }
+  for (auto &alien : aliens) {
+    alien.draw();
+  }
 }
 
 std::vector<Alien> create_aliens() {
@@ -53,9 +53,9 @@ std::vector<Alien> create_aliens() {
 
       int alien_type = 1;
 
-      if (row > 3) {
+      if (row == 0) {
         alien_type = 3;
-      } else if (row > 1) {
+      } else if (row == 1 || row == 2) {
         alien_type = 2;
       }
       float x = column * 55;
@@ -70,10 +70,6 @@ void game_initialize() {
   obstacles = obstacle_create();
   aliens = create_aliens();
 }
-
-
-
-
 
 // To avoid cross referencing we put the delete laser function in the game
 // header file
