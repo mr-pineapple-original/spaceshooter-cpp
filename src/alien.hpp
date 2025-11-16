a93998e07bc2246b85b549f2e495e4a802bdc757
Bongo <bongopoyo@proton.me>
2025-11-16 09:34:05 +0500

    Static memory

--- src/alien.hpp
+++ src/alien.hpp
@@ -2,22 +2,23 @@
 #include <raylib.h>
 
 static float aliens_speed = 1.0f;
-struct Alien
-{
+struct Alien {
   static Texture2D image[3];
   int type;
   Vector2 position;
 
-  Alien(int i_type, Vector2 i_position)
-  {
+  Alien() {
+    type = 1;
+    position = {-100, -100};
+  }
+  Alien(int i_type, Vector2 i_position) {
     position = i_position;
     type = i_type;
 
     if (image[type - 1].id == 0) // To only load the enemy aliens from the
                                  // memory if they haven't been loaded already
     {
-      switch (type)
-      {
+      switch (type) {
       case 1:
         image[0] = LoadTexture("sprites/alien-1.png");
         break;
@@ -49,30 +50,28 @@ struct Alien
     // }
   }
 
-  void uninitalize()
-  {
-    for (int i = 0; i != 4; i++)
-    {
+  void uninitalize() {
+    for (int i = 0; i != 4; i++) {
       UnloadTexture(image[i]); // To unload all the types of the aliens
     }
   }
 
-  void update(int alien_direction)
-  {
-    position.x = position.x + alien_direction * aliens_speed; 
+  void update(int alien_direction) {
+    position.x = position.x + alien_direction * aliens_speed;
   }
 
   // Draw the spaceship from the spaceship header file
-  void draw()
-  { 
-    DrawTextureV(image[type - 1], position, WHITE); 
-  }
+  void draw() { DrawTextureV(image[type - 1], position, WHITE); }
 
   int get_type() { return type; }
 
-  Rectangle get_rect()
-  {
+  Rectangle get_rect() {
     return {position.x, position.y, float(image[type - 1].width),
             float(image[type - 1].height)};
+  }
+
+  bool operator==(const Alien &other) const {
+    return position.x == other.position.x && position.y == other.position.y &&
+           type == other.type;
   }
 };
--- src/game.hpp
+++ src/game.hpp
@@ -2,22 +2,24 @@
 #include "alien.hpp"
 #include "obstacle.hpp"  // To include the obstacle file's struct
 #include "spaceship.hpp" // To include spaceship file's functions
-#include <fstream>       // To store highscore in a text file so we load the text from it everytime the game is opened
+#include <fstream> // To store highscore in a text file so we load the text from it everytime the game is opened
 #include <raylib.h>
 
-int alien_direction = 1;                            // Global variable to use it in the move_alien function and passing it as
-                                                    // an argument to update() function in the alien.hpp file
-constexpr static float alien_laser_interval = 0.35; // Global variable to calculate the laser shooting interval by aliens
+int alien_direction =
+    1; // Global variable to use it in the move_alien function and passing it as
+       // an argument to update() function in the alien.hpp file
+constexpr static float alien_laser_interval =
+    0.35; // Global variable to calculate the laser shooting interval by aliens
 float alien_fired_last_time = 0.0;
 
 Obstacle obstacles[5];
-// std::vector<Obstacle> obstacles; // A universial vector variable to store obstacles so we can
-// store it and use it in game_initialize function
-Alien[45] aliens;
-Laser alien_lasers[100];
+// std::vector<Obstacle> obstacles; // A universial vector variable to store
+// obstacles so we can store it and use it in game_initialize function
+Alien aliens[45];
+std::vector<Laser> alien_lasers;
 
 int spaceship_health = 3;
-bool game_run;              // To display the Game Over screen once it becomes false
+bool game_run; // To display the Game Over screen once it becomes false
 bool game_is_ended = false; // To end the game after level 5
 bool game_is_paused = false;
 int game_current_level = 1;
@@ -30,8 +32,7 @@ Sound spaceship_damage;      // To use this variable to open player_hurt
 
 // To avoid cross referencing we put the create obstacle function in the game
 // header file
-void create_obstacles()
-{
+void create_obstacles() {
   int obstacle_width = Obstacle({100, 100}).grid[0].size() * 3;
   // Takes the gap between two obstacles by taking the width of all
   // the 4 obstacles and subtracting it from the screen width and
@@ -40,9 +41,10 @@ void create_obstacles()
   // game_running it exactly 4 times to create 4
   // objects and later draw them
   float obstacle_gap = (GetScreenWidth() - (4 * obstacle_width)) / 5.0f;
-  for (int i = 0; i != 4; i++)
-  {
-    float obstacle_position_x =(i + 1) * obstacle_gap + i * obstacle_width; // To calculate the x position of the obstacle by
+  for (int i = 0; i != 4; i++) {
+    float obstacle_position_x =
+        (i + 1) * obstacle_gap +
+        i * obstacle_width; // To calculate the x position of the obstacle by
                             // ensuring they are evenly spaced
     float obstacle_position_y = GetScreenHeight() - 100;
 
@@ -52,70 +54,58 @@ void create_obstacles()
 
 void check_highscore() // To check for highscore
 {
-  if (score > highscore)
-  {
+  if (score > highscore) {
     highscore = score;
   }
 }
 
-void save_highscore_in_file(int highscore)
-{
+void save_highscore_in_file(int highscore) {
   std::ofstream highscore_file("highscore.txt");
-  if (highscore_file.is_open())
-  {
+  if (highscore_file.is_open()) {
     highscore_file
         << highscore; // Write the current highscore in the highscore_file file
                       // pointer to update the value in the highscore.txt file
     highscore_file.close();
-  }
-  else
-  {
+  } else {
     std::cerr << "Failed to save highscore to file" << std::endl;
   }
 }
 
-void game_end()
-{
+void game_end() {
   std::cout << "Game Completed" << std::endl;
   game_is_ended = true;
   game_run = true;
   save_highscore_in_file(highscore);
 }
 
-int load_highscore_from_file()
-{
+int load_highscore_from_file() {
   int loaded_highscore = 0;
   std::ifstream highscore_file("highscore.txt");
-  if (highscore_file.is_open())
-  {
+  if (highscore_file.is_open()) {
     highscore_file >> loaded_highscore;
-  }
-  else
-  {
+  } else {
     std::cerr << "Failed to load highscore from file." << std::endl;
   }
   return loaded_highscore;
 }
 
-void game_uninitialize()
-{
-  for (auto &alien : aliens)
-  {
-    alien.uninitalize();
-    aliens.pop_back();
-    UnloadMusicStream(music);
-    UnloadSound(enemy_death_sound);
-    UnloadSound(spaceship_death_sound);
-    UnloadSound(spaceship_damage);
+void game_uninitialize() {
+  for (int i = 0; i <= 45; i++) {
+    aliens[i] = Alien();
+    aliens[i].uninitalize();
   }
+  UnloadMusicStream(music);
+  UnloadSound(enemy_death_sound);
+  UnloadSound(spaceship_death_sound);
+  UnloadSound(spaceship_damage);
 }
-void game_draw()
-{
+void game_draw() {
 
   std::string health_text = "Health: " + std::to_string(spaceship_health);
   std::string score_text = "Score: " + std::to_string(score);
   std::string highscore_text = "High-Score: " + std::to_string(highscore);
-  std::string level_text = "Current Level: " + std::to_string(game_current_level);
+  std::string level_text =
+      "Current Level: " + std::to_string(game_current_level);
 
   DrawText(health_text.c_str(), 0, 0, 20, RED);
   DrawText(level_text.c_str(), 0, 680, 20, GREEN);
@@ -123,20 +113,17 @@ void game_draw()
   DrawText(score_text.c_str(), 630, 0, 20, YELLOW);
   DrawText(highscore_text.c_str(), 630 / 2, 0, 20, BLUE);
 
-  if (game_is_paused)
-  {
+  if (game_is_paused) {
     DrawText("Game Paused", (GetScreenWidth() / 2) - 120,
              (GetScreenHeight() / 2) - 20, 40, LIGHTGRAY);
   }
-  if (!game_run)
-  {
+  if (!game_run) {
     ClearBackground(BLACK);
 
     DrawText("You lose! Press 'Enter' to restart", 15,
              (GetScreenHeight() / 2) - 20, 40, LIGHTGRAY);
   }
-  if (game_is_ended)
-  {
+  if (game_is_ended) {
     ClearBackground(BLACK);
 
     DrawText("You Win!! Press 'Enter' to restart", 15,
@@ -145,70 +132,59 @@ void game_draw()
 
   spaceship_draw(); // Draw the spaceship from the spaceship header file
   //
-  for (auto &laser : spaceship_lasers)
-  {
+  for (auto &laser : spaceship_lasers) {
     laser.draw();
   }
-  for (auto &obstacle : obstacles)
-  {
+  for (auto &obstacle : obstacles) {
     obstacle.draw();
   }
-  for (auto &alien : aliens)
-  {
+  for (auto &alien : aliens) {
     alien.draw();
   }
-  for (auto &laser : alien_lasers)
-  {
+  for (auto &laser : alien_lasers) {
     laser.draw();
   }
 }
 
-std::vector<Alien> create_aliens()
-{
-  std::vector<Alien> temp_aliens;
-  for (int row = 0; row < 5; row++)
-  {
-    for (int column = 0; column < 11; column++)
-    {
+void create_aliens() {
+  int i = 0;
+  for (int row = 0; row < 5; row++) {
+    for (int column = 0; column < 11; column++) {
 
       int alien_type = 1;
 
-      if (row == 0)
-      {
+      if (row == 0) {
         alien_type = 3;
-      }
-      else if (row == 1 || row == 2)
-      {
+      } else if (row == 1 || row == 2) {
         alien_type = 2;
       }
       float x = column * 55;
       float y = row * 55;
-      temp_aliens.push_back(Alien(alien_type, {x + 75, y + 110}));
+      aliens[i] = Alien(alien_type, {x + 75, y + 110});
+      i++;
     }
   }
-  return temp_aliens;
 }
 
 void alien_laser() // To shoot lasers from the aliens
 {
   double current_time = GetTime();
-  if (current_time - alien_fired_last_time >= alien_laser_interval &&
-      !aliens.empty())
-  {
-    int random_value = GetRandomValue(0, aliens.size() - 1);
-    Alien &alien = aliens[random_value];
-    alien_lasers.push_back(Laser({alien.position.x + alien.image[alien.type - 1].width / 2,
-                                  alien.position.y + alien.image[alien.type - 1].height},
-                                 6));
+  if (current_time - alien_fired_last_time >= alien_laser_interval) {
+    int random_value = GetRandomValue(0, 45 - 1);
+    Alien alien = aliens[random_value];
+    alien_lasers.push_back(
+        Laser({alien.position.x + alien.image[alien.type - 1].width / 2,
+               alien.position.y + alien.image[alien.type - 1].height},
+              6));
     alien_fired_last_time = GetTime();
   }
 }
 
-void alien_move_down(int distance) // To move the aliens down in the game.hpp file by calling
-                                   // this function in the alien_move function
+void alien_move_down(
+    int distance) // To move the aliens down in the game.hpp file by calling
+                  // this function in the alien_move function
 {
-  for (auto &alien : aliens)
-  {
+  for (auto &alien : aliens) {
     alien.position.y = alien.position.y + distance;
   }
 }
@@ -216,8 +192,7 @@ void alien_move_down(int distance) // To move the aliens down in the game.hpp fi
 void alien_move() // To move the aliens horizontally in the game.hpp file by
                   // calling a function update() from the alien struct
 {
-  for (auto &alien : aliens)
-  {
+  for (auto &alien : aliens) {
     if (alien.position.x + alien.image[alien.type - 1].width >
         GetScreenWidth()) // To avoid aliens getting out from the right side of
                           // the window screen
@@ -237,8 +212,7 @@ void alien_move() // To move the aliens horizontally in the game.hpp file by
   }
 }
 
-void game_initialize()
-{
+void game_initialize() {
   score = 0;
   game_current_level = 1;
   aliens_speed = 1;
@@ -253,144 +227,117 @@ void game_initialize()
   PlayMusicStream(music);
   spaceship_initialize();
   create_obstacles();
-  aliens = create_aliens();
+  create_aliens();
 }
 
 // To avoid cross referencing we put the delete laser function in the game
 // header file
-void laser_delete()
-{
+void laser_delete() {
   for (auto laser = spaceship_lasers.begin();
-       laser != spaceship_lasers.end();)
-  {
-    if (!laser->active)
-    {
+       laser != spaceship_lasers.end();) {
+    if (!laser->active) {
       laser = spaceship_lasers.erase(laser);
-    }
-    else
-    {
+    } else {
       ++laser;
     }
   }
 
-  for (auto laser = alien_lasers.begin(); laser != alien_lasers.end();)
-  {
-    if (!laser->active)
-    {
+  for (auto laser = alien_lasers.begin(); laser != alien_lasers.end();) {
+    if (!laser->active) {
       laser = alien_lasers.erase(laser);
-    }
-    else
-    {
+    } else {
       ++laser;
     }
   }
 }
 
-void game_over()
-{
+void game_over() {
   std::cout << "Game Over" << std::endl;
   PlaySound(spaceship_death_sound);
   game_run = false;
   save_highscore_in_file(highscore);
 }
 
-void game_level_completed()
-{
-  if (game_current_level == 5)
-  {
+void game_level_completed() {
+  if (game_current_level == 5) {
     game_end();
-  }
-  else
-  {
+  } else {
     std::cout << "Level completed" << endl;
     aliens_speed += 0.3;
     spaceship_fire_delay -= 0.02;
     game_current_level += 1;
-    aliens = create_aliens();
+    create_aliens();
     create_obstacles();
   }
 }
-void check_for_collisions()
-{
+
+bool aliens_is_empty() {
+  bool flag = true;
+  for (int i = 0; i <= 44; i++) {
+    if (!(aliens[i] == Alien())) {
+      flag = false;
+    }
+  }
+  return flag;
+}
+
+void check_for_collisions() {
   // Spaceship lasers
-  for (auto &laser : spaceship_lasers)
-  {
-    auto it = aliens.begin();
-    while (it != aliens.end())
-    {
-      if (CheckCollisionRecs(it->get_rect(), laser.get_rect()))
-      {
+  for (auto &laser : spaceship_lasers) {
+
+    for (int i = 0; i <= 44; i++) {
+      if (CheckCollisionRecs(aliens[i].get_rect(), laser.get_rect())) {
         PlaySound(enemy_death_sound);
-        if (it->type == 1)
-        {
+        if (aliens[i].type == 1) {
           score = score + 100;
         }
-        if (it->type == 2)
-        {
+        if (aliens[i].type == 2) {
           score = score + 200;
         }
-        if (it->type == 3)
-        {
+        if (aliens[i].type == 3) {
           score = score + 300;
         }
         check_highscore();
-        it = aliens.erase(it);
+        // Delete the alien.
+        aliens[i] = Alien();
 
         laser.active = false;
       }
-      else
-      {
-        ++it;
-      }
     }
 
-    for (auto &obstacle : obstacles)
-    {
+    for (auto &obstacle : obstacles) {
       auto it = obstacle.blocks.begin();
-      while (it != obstacle.blocks.end())
-      {
-        if (CheckCollisionRecs(it->get_rect(), laser.get_rect()))
-        {
+      while (it != obstacle.blocks.end()) {
+        if (CheckCollisionRecs(it->get_rect(), laser.get_rect())) {
           it = obstacle.blocks.erase(it);
           laser.active = false;
-        }
-        else
-        {
+        } else {
           ++it;
         }
       }
     }
   }
 
-  if (aliens.empty())
-  {
+  if (aliens_is_empty()) {
     game_level_completed();
   }
   // Alien lasers
-  for (auto &laser : alien_lasers)
-  {
-    if (CheckCollisionRecs(laser.get_rect(), spaceship_get_rect()))
-    {
+  for (auto &laser : alien_lasers) {
+    if (CheckCollisionRecs(laser.get_rect(), spaceship_get_rect())) {
       laser.active = false;
       PlaySound(spaceship_damage);
       spaceship_health -= 1;
-      if (spaceship_health == 0)
-      {
+      if (spaceship_health == 0) {
         game_over();
       }
     }
-    for (auto &obstacle : obstacles)
-    {
+    for (auto &obstacle : obstacles) {
       auto it = obstacle.blocks.begin();
-      while (it != obstacle.blocks.end())
-      {
-        if (CheckCollisionRecs(it->get_rect(), laser.get_rect()))
-        {
+      while (it != obstacle.blocks.end()) {
+        if (CheckCollisionRecs(it->get_rect(), laser.get_rect())) {
           it = obstacle.blocks.erase(it);
           laser.active = false;
-        }
-        else
-        {
+        } else {
           ++it;
         }
       }
@@ -398,79 +345,69 @@ void check_for_collisions()
   }
 
   // Aliens
-  for (auto &alien : aliens)
-  {
-    for (auto &obstacle : obstacles)
-    {
+  for (auto &alien : aliens) {
+    for (auto &obstacle : obstacles) {
       auto it = obstacle.blocks.begin();
-      while (it != obstacle.blocks.end())
-      {
-        if (CheckCollisionRecs(it->get_rect(), alien.get_rect()))
-        {
+      while (it != obstacle.blocks.end()) {
+        if (CheckCollisionRecs(it->get_rect(), alien.get_rect())) {
           it = obstacle.blocks.erase(it);
-        }
-        else
-        {
+        } else {
           ++it;
         }
       }
     }
 
-    if (CheckCollisionRecs(alien.get_rect(), spaceship_get_rect()))
-    {
+    if (CheckCollisionRecs(alien.get_rect(), spaceship_get_rect())) {
       game_over();
     }
   }
 }
-void game_update()
-{
-  if (!game_run)
-  {
+void game_update() {
+  if (!game_run) {
     return;
   }
 
-  if (game_is_ended)
-  {
+  if (game_is_ended) {
     return;
   }
-  if (game_is_paused)
-  {
+  if (game_is_paused) {
     return;
   }
 
   UpdateMusicStream(music);
   spaceship_boundaries();
 
-  for (auto &laser : spaceship_lasers)
-  {
+  for (auto &laser : spaceship_lasers) {
     laser.update();
   }
   laser_delete();
 
-  for (auto &obstacle : obstacles)
-  {
+  for (auto &obstacle : obstacles) {
     obstacle.draw();
   }
   alien_laser();
-  for (auto &laser : alien_lasers)
-  {
+  for (auto &laser : alien_lasers) {
     laser.update();
   }
   alien_move();
   check_for_collisions();
 }
 
-void game_reset()
-{
+void aliens_clear() {
+  for (int i = 0; i <= 44; i++) {
+    aliens[i] = Alien();
+  }
+}
+
+void game_reset() {
   // delete everything
   spaceship_reset();
-  aliens.clear();
+  aliens_clear();
   alien_lasers.clear();
   // obstacles.clear();
 
   // bad way to clear obstacles
-  for (int i = 0; i < 5; i++)
-  {
+  for (int i = 0; i < 5; i++) {
     obstacles[i] = Obstacle({-100, -100});
   }
 
@@ -478,55 +415,42 @@ void game_reset()
   game_initialize();
 }
 
-void handle_input()
-{
-  if (game_is_ended)
-  {
-    if (IsKeyDown(KEY_ENTER))
-    {
+void handle_input() {
+  if (game_is_ended) {
+    if (IsKeyDown(KEY_ENTER)) {
       save_highscore_in_file(highscore);
       game_is_ended = false;
       game_reset();
     }
   }
-  if (!game_run)
-  {
-    if (IsKeyDown(KEY_ENTER))
-    {
+  if (!game_run) {
+    if (IsKeyDown(KEY_ENTER)) {
       save_highscore_in_file(highscore);
       game_reset();
     }
-    if (IsKeyDown(KEY_Q))
-    {
+    if (IsKeyDown(KEY_Q)) {
       save_highscore_in_file(highscore);
       CloseWindow();
     }
     return;
   }
-  if (IsKeyDown(KEY_LEFT))
-  {
+  if (IsKeyDown(KEY_LEFT)) {
     spaceship_position.x = spaceship_position.x - 5;
-  }
-  else if (IsKeyDown(KEY_RIGHT))
-  {
+  } else if (IsKeyDown(KEY_RIGHT)) {
     spaceship_position.x = spaceship_position.x + 5;
   }
-  if (IsKeyDown(KEY_SPACE))
-  {
+  if (IsKeyDown(KEY_SPACE)) {
     spaceship_fire_laser();
   }
 
-  if (IsKeyDown(KEY_Q))
-  {
+  if (IsKeyDown(KEY_Q)) {
     save_highscore_in_file(highscore);
     CloseWindow();
   }
-  if (IsKeyPressed(KEY_P))
-  {
+  if (IsKeyPressed(KEY_P)) {
     game_is_paused = !game_is_paused;
   }
-  if (IsKeyPressed(KEY_N))
-  {
+  if (IsKeyPressed(KEY_N)) {
     game_level_completed();
   }
 }
